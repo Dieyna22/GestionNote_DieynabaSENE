8f9cb349cd9453f01a9efad37b6878516d1642a6
Dieyna22 <101632839+Dieyna22@users.noreply.github.com>
2023-12-26 08:09:42 +0000

    ajout test unitaie

--- gestionNote/src/app/administrateur/gestion-classes/gestion-classes.component.spec.ts
+++ gestionNote/src/app/administrateur/gestion-classes/gestion-classes.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
 import { GestionClassesComponent } from './gestion-classes.component';
 
 describe('GestionClassesComponent', () => {
@@ -8,14 +8,38 @@ describe('GestionClassesComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [GestionClassesComponent]
+      declarations: [GestionClassesComponent],
+      imports: [FormsModule],
     });
+
     fixture = TestBed.createComponent(GestionClassesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('doit créer le composant', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('doit ajouter une classe lorsque ajoutClassesValider est appelé avec un nom valide', () => {
+    component.nomClasse = 'New Class';
+    const previousLength = component.tabClasses.length;
+    component.ajoutClassesValider();
+    expect(component.tabClasses.length).toEqual(previousLength + 1);
+  });
+
+  it('ne doit pas ajouter de classe lorsque ajoutClassesValider est appelé avec un nom vide', () => {
+    component.nomClasse = '';
+    const previousLength = component.tabClasses.length;
+    component.ajoutClassesValider();
+    expect(component.tabClasses.length).toEqual(previousLength);
+  });
+
+  it('doit mettre à jour le nom de la classe lorsque modierClasse est appelé', () => {
+    const originalName = 'Original Class';
+    component.currentClasse = { idClasse: 1, nom: originalName };
+    component.nom = 'Updated Class';
+    component.modierClasse();
+    expect(component.currentClasse.nom).toEqual('Updated Class');
   });
 });
--- gestionNote/src/app/login/login.component.spec.ts
+++ gestionNote/src/app/login/login.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './login.component';
+import { timer } from 'rxjs';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,14 +10,38 @@ describe('LoginComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [LoginComponent]
+      declarations: [LoginComponent],
+      imports: [FormsModule, RouterTestingModule],
     });
+
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('doit créer le composant', () => {
     expect(component).toBeTruthy();
   });
+
+  it('devrait valider le modèle d’e-mail', () => {
+    component.emailLogin = 'invalid-email';
+    component.passwordLogin = 'valid-password';
+    component.connexion();
+    expect(component.alertMessage).toHaveBeenCalledWith('error', 'Attention', 'Veillez revoir votre email', timer);
+  });
+
+  it('doit valider la longueur du mot de passe', () => {
+    component.emailLogin = 'valid-email@example.com';
+    component.passwordLogin = 'short';
+    component.connexion();
+    expect(component.alertMessage).toHaveBeenCalledWith('error', 'Attention', 'Le mot de passe doit contenir plus de huit caractéres', timer);
+  });
+
+  // Add more test cases based on your requirements
+
+  // Mock the alertMessage function
+  beforeEach(() => {
+    spyOn(component, 'alertMessage');
+  });
 });
+
